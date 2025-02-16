729aa93eb739318b02f5d16034bb7b52b4cf3b86
Shreeyam Kacker <shreeyamkacker@gmail.com>
2025-02-15 20:54:27 -0500

    configure proxy for dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        // Optionally, if you need to remove `/api` from the forwarded request:
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});

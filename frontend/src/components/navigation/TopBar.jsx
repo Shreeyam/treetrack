bebc14da58f0ecf5767eb33f790fd12394f6de60
Shreeyam Kacker <shreeyamkacker@gmail.com>
2025-04-20 11:41:02 -0700

    push version

--- frontend/src/components/navigation/TopBar.jsx
+++ frontend/src/components/navigation/TopBar.jsx
@@ -157,7 +157,7 @@ const TopBar = memo(({
                         </DropdownMenuItem>
                         <DropdownMenuSeparator />
                         <DropdownMenuItem disabled className="opacity-50">
-                            Treetrack v0.1.0<br />{BUILD_DATE}
+                            Treetrack v0.1.1<br />{BUILD_DATE}
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>

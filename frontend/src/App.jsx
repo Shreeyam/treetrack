58af23d2681cd1acd16b9325034d1974e7737534
Labtec <labtec901@gmail.com>
2025-04-21 18:19:02 -0700

    Set position to null for TopBar add button to enable cascading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -282,7 +282,7 @@ function App({user, setUser}) {
             setLastNodePosition, 
             setNewTaskTitle, 
             setNodes,
-            position
+            position: null // Force position to be null for TopBar add button to enable cascading
         })(),
         [newTaskTitle, currentProject, reactFlowInstance, reactFlowWrapper, lastNodePosition, cascadeCount, cascadeStartPoint, createNodeStyle]
     );

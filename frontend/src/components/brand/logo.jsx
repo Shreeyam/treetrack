c172fc4cc902ba47d7d24062975587d8733fbea6
Shreeyam Kacker <shreeyamkacker@gmail.com>
2025-05-04 16:04:05 -0400

    adjust logo size

--- frontend/src/components/brand/logo.jsx
+++ frontend/src/components/brand/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <div className="flex items-center">
             <a href="/" className="flex items-center gap-1.5">
-                <img src="/treetracklogoSMALL.svg" alt="Treetrack Logo" className="h-8" /> {/* Added alt attribute */}
+                <img src="/treetracklogoSMALL.svg" alt="Treetrack Logo" className="h-7" /> {/* Added alt attribute */}
                 <span className="text-xl font-bold">Treetrack</span>
             </a>
         </div>

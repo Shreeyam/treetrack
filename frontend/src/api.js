3b084e7d877d19e8388d66211d76aa11de116f2e
Labtec <labtec901@gmail.com>
2025-05-11 11:50:22 -0700

    Remove unused API functions and commented-out code for clarity

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,33 +1,4 @@
 // src/api.js
-//const API_BASE = 'http://localhost:3001';
-
-// export const fetchTasks = async () => {
-//   const res = await fetch(`${API_BASE}/tasks`);
-//   return res.json();
-// };
-
-// export const fetchDependencies = async () => {
-//   const res = await fetch(`${API_BASE}/dependencies`);
-//   return res.json();
-// };
-
-// export const createTask = async (title, description) => {
-//   const res = await fetch(`${API_BASE}/tasks`, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ title, description }),
-//   });
-//   return res.json();
-// };
-
-// export const createDependency = async (child, parent) => {
-//   const res = await fetch(`${API_BASE}/dependencies`, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ child, parent }),
-//   });
-//   return res.json();
-// };
 
 export const fetchUser = async () => {
   const res = await fetch('/api/me', { credentials: 'include' });
@@ -57,29 +28,6 @@ export const deleteProject = async (projectId) => {
   return res.json();
 };
 
-// export const updateTask = async (taskId, taskData) => {
-//   await fetch(`/api/tasks/${taskId}`, {
-//     method: 'PUT',
-//     credentials: 'include',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(taskData),
-//   });
-// };
-
-// export const deleteTask = async (taskId) => {
-//   await fetch(`/api/tasks/${taskId}`, {
-//     method: 'DELETE',
-//     credentials: 'include',
-//   });
-// };
-
-// export const deleteDependency = async (dependencyId) => {
-//   await fetch(`/api/dependencies/${dependencyId}`, {
-//     method: 'DELETE',
-//     credentials: 'include',
-//   });
-// };
-
 export const generate = async (userInput, projectId, currentState) => {
   const res = await fetch('/api/generate', {
     method: 'POST',
@@ -89,11 +37,3 @@ export const generate = async (userInput, projectId, currentState) => {
   });
   return res.json();
 };
-
-// export const fetchProjectState = async (projectId) => {
-//   const tasksRes = await fetch(`/api/tasks?project_id=${projectId}`, { credentials: 'include' });
-//   const depRes   = await fetch(`/api/dependencies?project_id=${projectId}`, { credentials: 'include' });
-//   const tasks    = (await tasksRes.json()).tasks;
-//   const deps     = (await depRes.json()).dependencies;
-//   return { tasks, dependencies: deps };
-// };
